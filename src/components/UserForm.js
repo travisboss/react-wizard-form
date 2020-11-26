48186a8707dfba0ffe0d7bbb28d3dccc5c1069b3
Travis Boss <travisboss@gmail.com>
2020-11-26 00:54:57 +0000

    add FormPersonalDetails component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails'
+import FormPersonalDetails from './FormPersonalDetails'
 
 export class UserForm extends Component {
   state = {
@@ -16,7 +18,7 @@ export class UserForm extends Component {
   nextStep = () => {
     const { step } = this.state;
     this.setState({
-      setp: step + 1
+      step: step + 1
     })
   }
 
@@ -24,7 +26,7 @@ export class UserForm extends Component {
   prevStep = () => {
     const { step } = this.state;
     this.setState({
-      setp: step - 1
+      step: step - 1
     })
   }
 
@@ -48,10 +50,17 @@ export class UserForm extends Component {
           />
         )
       case 2:
-        return <h1>FormPersonalDetail</h1>
+        return (
+          <FormPersonalDetails
+            nextStep ={this.nextStep}
+            prevStep ={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
       case 3:
         return <h1>Confirm</h1>
-      case 3:
+      case 4:
         return <h1>Success</h1>
     }
 
